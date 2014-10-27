b73025a8ae8142b44c95d2e15f84f74104e99abc
elena <elena.15@hotmail.it>
2014-10-27 19:28:22 +0000

    frame has 2 rows and when a ball hit the pins the score change

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,8 +1,21 @@
 describe('Frame', function(){
 
-	it('should have an initial score of 10', function(){
-		var frame = new Frame();
-		expect(frame.score).toEqual(10);
-	});
+	describe('A new frame: ', function(){
 
+		it('should have an initial score of 10', function(){
+			var frame = new Frame();
+			expect(frame.score).toEqual(10);
+		});
+
+		it('should have 2 rows', function(){
+			var frame = new Frame();
+			expect(frame.rows).toEqual(2);
+		});
+
+		it('should change the score when a ball hit the pins', function(){
+			var frame = new Frame();
+			frame.ballHit(3);
+			expect(frame.score).toEqual(7);
+		});
+	});
 });
--- src/frame.js
+++ src/frame.js
@@ -1,4 +1,11 @@
 function Frame (){
 	this.score = 10;
-	this.rows =  2;
+	this.rows = 2;
+
+	this.ballHit = function(number){
+		return this.score -= number;
+	};
 };
+
+
+
